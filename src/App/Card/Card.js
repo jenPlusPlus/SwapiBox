d58db92c1a8eb913f8ae4f168f66f1e991ee1e77
Jennifer Woodson <jenniferpwoodson@gmail.com>
2017-10-11 20:57:19 -0600

    map over array of residents and species

--- src/App/Card/Card.js
+++ src/App/Card/Card.js
@@ -2,12 +2,15 @@ import React from 'react';
 
 const Card = ({ cardData, cardType }) => {
   if (cardType === 'people') {
+    const mappedSpecies = cardData.species.map((species, index) => {
+      return <li key={index + Date.now()}>{species}</li>;
+    });
     return (
       <div className='card'>
         <h3 className='people-name'>Name: <span>{cardData.name}</span></h3>
         <button className='favorite'>Fave</button>
         <p className='people-homeworld'>Homeworld: <span>{cardData.homeworld}</span></p>
-        <p className='people-species'>Species: <span>{cardData.species}</span></p>
+        <p className='people-species'>Species: <ul>{mappedSpecies}</ul></p>
         <p className='people-homeworld-population'>Homeworld Population: <span>{cardData.homeworldPopulation}</span></p>
       </div>
     );
@@ -22,6 +25,9 @@ const Card = ({ cardData, cardType }) => {
       </div>
     );
   } else if (cardType === 'planets') {
+    const mappedResidents = cardData.residents.map((resident, index) => {
+      return <li key={index + Date.now()}>{resident}</li>;
+    });
     return (
       <div className='card'>
         <h3 className='planets-name'>Name: <span>{cardData.name}</span></h3>
@@ -29,8 +35,8 @@ const Card = ({ cardData, cardType }) => {
         <p className='planets-terrain'>Terrain: <span>{cardData.terrain}</span></p>
         <p className='planets-population'>Population: <span>{cardData.population}</span></p>
         <p className='planets-climate'>Climate: <span>{cardData.climate}</span></p>
-        <p className='planets-residents'>Residents: <span>{cardData.residents}</span></p>
-      </div>
+          <p className='planets-residents'>Residents: <ul>{mappedResidents}</ul></p>
+        </div>
     );
   }
 };
