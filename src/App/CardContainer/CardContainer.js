81ee8800eb4640368ab071a389af168763ad0dbe
Jennifer Woodson <jenniferpwoodson@gmail.com>
2017-10-11 19:54:28 -0600

    test child component render

--- src/App/CardContainer/CardContainer.js
+++ src/App/CardContainer/CardContainer.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Card from '../Card/Card'
 
-const CardContainer = (props) => {
+const CardContainer = ({ cardData }) => {
 
   return (
     <div>
+      Card Container!
+      <Card cardData={cardData} />
     </div>
   )
 };
--- src/App/Header/Button/Button.js
+++ src/App/Header/Button/Button.js
@@ -1,10 +1,9 @@
 import React from 'react'
 
 const Button = ({ buttonTitle, buttonAction}) => {
-
   return (
     <div>
-      <a href={`/${buttonTitle}`}>
+      <a href={`/${buttonTitle.toLowerCase()}`}>
         <button>
           {buttonTitle}
         </button>
